1e64a82d41548633e182fa7d98f2641bbda119ae
Pranav Ajay <pranavajay2002@gmail.com>
2022-02-07 02:05:12 +0530

    Added blogpost model

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,19 @@
+
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    author_name = models.CharField(max_length=100)
+    upload_time = models.DateTimeField(auto_now_add=True)
+    post_text = models.TextField()
+    post_title = models.CharField(max_length=100,unique=True)
+
+
+    def __str__(self) -> str:
+        return self.post_title
+
+
+
+
+
