bf03150692d8fb9941946da7344f9ce6cbb597fa
Pranav Ajay <pranavajay2002@gmail.com>
2022-02-07 15:51:34 +0530

    Added blog card

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('blog/',views.blog,name="blog")
+    path('blog/',views.blog,name="blog"),
+    path('bloglist/',views.blog_list,name="bloglist")
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -11,3 +11,6 @@ def blog(req):
     return render(req,'BlogView.html',context={'data':context_list})
 
     
+def blog_list(req):
+    context_list = models.BlogPost.objects.order_by('upload_time')
+    return render(req,'BlogList.html',context={'data':context_list})
